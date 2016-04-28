fac2baf1cb1bf3e0df1cad41837a9cbff6bbc7ad
Andrew Bernard <andrew.bernard@gmail.com>
2016-04-28 13:22:51 +1000

    added manpages

--- debian/lys.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the lys package
-#
-0 4	* * *	root	[ -x /usr/bin/lys_maintenance ] && /usr/bin/lys_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(lys):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="lys" command="/usr/bin/lys"
